7ed828847b12f641022ffb3408b24bb8dbad19b3
Trinkku <trinh.leloan@hotmail.com>
2022-11-06 14:56:42 +0200

    Map update
    Klikatessa markeriin näkyy myymälän tiedot

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -12,14 +12,15 @@ export default function MapScreen({navigation}) {
 
 const [latitude, setLatitude] = useState(0)
 const [longitude, setLongitude] = useState(0)
+const [desc, setDesc] = useState(0)
 const [isLoading, setIsLoading] = useState(true) //koska useEffect jossa haetaan sijaintia saattaa hakea tietoa hitaasti. 
 
 //Tämän isLoading tilamuuttujan avulla voidaan tehdä toiminnallisuus mikä antaa erillisen odotustilan
 const [markers, setMarkers] = useState([ //tänne tallentuu markereita longpressillä 
-{"latitude": 65.00, "longitude": 25.480, "title": 'testi'},
-{"latitude": 64.20, "longitude": 24.580, "title": 'testi'},
-{"latitude": 60.20, "longitude": 25.06, "title": 'testi'},
-{"latitude": 60.40, "longitude": 25.15, "title": 'testi'},
+{"latitude": 65.00, "longitude": 25.480, "title": 'Oulun myymälä, puh. 010 2448756'},
+{"latitude": 64.20, "longitude": 24.580, "title": 'Tehtaanmyymälä, puh. 010 2448756'},
+{"latitude": 60.20, "longitude": 25.06, "title": ' Helsingin myymälä, puh. 010 2448756'},
+{"latitude": 60.40, "longitude": 25.15, "title": 'Keravan myymälä, puh. 010 2448756'},
 
 
 ])
@@ -35,56 +36,17 @@ useEffect(() => {
   })
 }, [])
 
-//Tallennetaan JSON tietoja koneen muistiin 
-const storeData = async (value) => {
-  try {
-    const jsonValue = JSON.stringify(value) //Taulukko muutetaan json kieleksi
-    await AsyncStorage.setItem(STORAGE_KEY, jsonValue) //jsonValue on markers taulukko(määr. returnin alla). siirretään  tietokannan (storage_key) avaimen alle.
-  } catch (error) {
-    console.log(error)
-    // saving error
-  }
-}
-//Luetaan tallennettu data
-const getData = async() => {
-  try{
-    return AsyncStorage.getItem(STORAGE_KEY) //haetaan avaimella
-    .then (response => JSON.parse(response)) //muunnetaan vastaus json takasin taulukoksi/objekteiksi
-    .then (data => {
-      if (data===null) data = [] //jos data on tyhjä eli null, silloin data on tyhjä taulukko
-
-      setMarkers(data) //jos on dataa niin laitetaan se setMarkesiin
-    }).catch (e =>{
-      console.log(e)
-    })
-  } catch (error){
-    console.log(error)
-  }
-}
-
-
 
   return (
     <View style={Styles.container}>
-    <MapView style={Styles.map} 
-    onLongPress={(e)=> {
-      const newMarker ={"lat" : e.nativeEvent.coordinate.latitude, "lng": e.nativeEvent.coordinate.longitude}
-      const updatedMarkers= [...markers, newMarker]
-      setMarkers(updatedMarkers)
-      storeData(updatedMarkers)
-    
-    }}
-    >
+    <MapView style={Styles.map}   >
 
 
     {markers.map((marker, index) => (
       <Marker 
       key={index}
       coordinate={{latitude:marker.latitude, longitude:marker.longitude}}
-    
-      
-  
-      />
+      title={marker.title}/>
 
     ))
     }
